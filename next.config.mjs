478827cc615c2b4e012458a1e4077705e9dab617
Kangmin Kim <76634341+amazon7737@users.noreply.github.com>
2025-02-15 01:04:03 +0900

    commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
+const prefix =
+  process.env.NODE_ENV === "production" ? "https://amazon7737.github.io/todolist/" : "";
+
 const nextConfig = {
   reactStrictMode: true,
+  output: "export",
 };
 
 export default nextConfig;
