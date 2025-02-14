05483732c017ec2126c8e366c6374add37b9fca0
Kangmin Kim <76634341+amazon7737@users.noreply.github.com>
2025-02-15 00:40:44 +0900

    commit

---
+++ src/components/Todo.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+export default function Todo() {
+  return (
+    <>
+      <div className="content">
+        <div className="cardlist">
+          <div className="card">card</div>
+          <div className="card">card</div>
+          <div className="card">card</div>
+          <div className="card">card</div>
+          <div className="card">card</div>
+          <div className="card">card</div>
+          <div className="card">card</div>
+        </div>
+        <div>hfff</div>
+        <div>hfff</div>
+        <div>hfff</div>
+        <div>hfff</div>
+        <div>hfff</div>
+        <div>hfff</div>
+      </div>
+    </>
+  );
+}
---
+++ src/pages/index.js
@@ -0,0 +1,9 @@
+import Todo from "@/components/Todo";
+
+export default function Home() {
+  return (
+    <div className="wrap">
+      <Todo />
+    </div>
+  );
+}
